4e2a6fd52f36058c1b56dd3c5d22c044acf2440d
Nathan Kau <nathankau@gmamil.com>
2025-01-30 13:38:34 -0800

    add scripts to add files to skip stages up until stage5

---
+++ add_skips.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Define the stages
+stages=("stage0" "stage1" "stage2" "stage3" "stage4")
+image_stages=("stage2" "stage4")
+
+# Loop through each stage and create a SKIP file
+for stage in "${stages[@]}"; do
+    touch "$stage/SKIP"
+done
+
+# Loop through each image stage and create a SKIP_IMAGES file
+for stage in "${image_stages[@]}"; do
+    touch "$stage/SKIP_IMAGES"
+done
